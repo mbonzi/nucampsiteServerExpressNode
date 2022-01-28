b7b7b5aa6adb2f205b969e452da3d1b00a73090a
Madelyn Bonzi <madelyn.bonzi@gmail.com>
2022-01-27 21:02:48 -0800

    updated campsiteSchema.

--- models/campsite.js
+++ models/campsite.js
@@ -33,6 +33,23 @@ const campsiteSchema = new Schema({
         type: String,
         required: true
     },
+    image: {
+        type: String,
+        required: true
+    },
+    elevation: {
+        type: Number,
+        required: true
+    },
+    cost: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
     comments: [commentSchema]
 }, {
     timestamps: true
