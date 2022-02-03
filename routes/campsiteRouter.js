75d0a5b36c3e5a8af9d0e09845116fd740c6383f
Madelyn Bonzi <madelyn.bonzi@gmail.com>
2022-02-02 18:55:45 -0800

    user authentication with passport

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -10,6 +10,7 @@ campsiteRouter
   .route("/")
   .get((req, res, next) => {
     Campsite.find()
+        .populate('comments.author')
       .then((campsites) => {
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
@@ -45,6 +46,7 @@ campsiteRouter
   .route("/:campsiteId")
   .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
       .then((campsite) => {
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
@@ -86,6 +88,7 @@ campsiteRouter
   campsiteRouter.route('/:campsiteId/comments')
   .get((req, res, next) => {
       Campsite.findById(req.params.campsiteId)
+      .populate('comments.author')
       .then(campsite => {
           if (campsite) {
               res.statusCode = 200;
@@ -149,6 +152,7 @@ campsiteRouter
   campsiteRouter.route('/:campsiteId/comments/:commentId')
   .get((req, res, next) => {
       Campsite.findById(req.params.campsiteId)
+      .populate('comments.author')
       .then(campsite => {
           if (campsite && campsite.comments.id(req.params.commentId)) {
               res.statusCode = 200;
