3696e612e3ebb25b4e78bac740479c15b333365b
Madelyn Bonzi <madelyn.bonzi@gmail.com>
2022-02-08 20:58:19 -0800

    updates to authentication

--- routes/users.js
+++ routes/users.js
@@ -56,7 +56,10 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
     const token = authenticate.getToken({_id: req.user._id});
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.json({success: true, token: token, status: 'You are successfully logged in!'});
+    res.json({
+        success: true, 
+        token: token, 
+        status: 'You are successfully logged in!'});
 });
 
 
